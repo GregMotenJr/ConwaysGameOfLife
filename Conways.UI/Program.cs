ebfcbf761b62650343fda0d2fd3d4e4709f5fd7d
gmoten <gmoten@QA-035022>
2018-03-20 14:26:29 -0400

    UI

---
+++ Conways.UI/Program.cs
@@ -0,0 +1,47 @@
+﻿using Conways.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conways.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grid = new LifeGrid(25, 70);
+            grid.Randomize();
+
+            ShowGrid(grid.CurrentState);
+
+            while (Console.ReadLine() != "q")
+            {
+                grid.UpdateState3();
+                ShowGrid(grid.CurrentState);
+            }
+        }
+
+        private static void ShowGrid(CellState[,] currentState)
+        {
+            int x = 0;
+            int rowLength = currentState.GetUpperBound(1) + 1;
+
+            var output = new StringBuilder();
+
+            foreach (var state in currentState)
+            {
+                output.Append(state == CellState.Alive ? "O" : "·");
+                x++;
+                if (x >= rowLength)
+                {
+                    x = 0;
+                    output.AppendLine();
+                }
+            }
+            Console.Clear();
+            Console.Write(output.ToString());
+        }
+    }
+}
